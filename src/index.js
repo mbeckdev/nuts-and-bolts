86672ec9b9950259d1a07a973e371ada88cddc2b
mbeckdev <mbeckdev42@gmail.com>
2021-06-04 14:35:05 -0600

    Add initial styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 'use strict';
-import css from './styles.css';
+// import css from './styles.css';
+import './styles.css';
 
 //GET PICTURES INTO THIS PROJECT WHEN YOU GET SOME PICTURES:
 // import MyPicture from './image.png';
@@ -13,3 +14,57 @@ import css from './styles.css';
 //   color: red;
 //   background: url('./image.png');
 // }
+
+import { loadPage } from './load-page.js';
+// import { loadHomeTab } from './page-home.js';
+// import { loadMenuTab } from './page-menu.js';
+// import { loadAboutTab } from './page-about.js';
+
+loadPage();
+// loadHomeTab();
+// loadMenuTab();
+// loadAboutTab();
+
+const mainTab = document.getElementById('main-tab');
+const menuTab = document.getElementById('menu-tab');
+const aboutTab = document.getElementById('about-tab');
+
+mainTab.addEventListener('click', function () {
+  clearAndSwitchTab(mainTab);
+});
+menuTab.addEventListener('click', function () {
+  clearAndSwitchTab(menuTab);
+});
+aboutTab.addEventListener('click', function () {
+  clearAndSwitchTab(aboutTab);
+});
+
+const copyContainer = document.getElementById('copy-container');
+function clearAndSwitchTab(tab) {
+  while (copyContainer.firstChild) {
+    copyContainer.removeChild(copyContainer.firstChild);
+  }
+
+  clearTabColors();
+  let theId = tab.id;
+  switch (theId) {
+    case 'main-tab':
+      mainTab.classList.add('tab-selected');
+      loadHomeTab();
+      break;
+    case 'menu-tab':
+      menuTab.classList.add('tab-selected');
+      loadMenuTab();
+      break;
+    case 'about-tab':
+      aboutTab.classList.add('tab-selected');
+      loadAboutTab();
+      break;
+  }
+}
+
+function clearTabColors() {
+  mainTab.classList.remove('tab-selected');
+  menuTab.classList.remove('tab-selected');
+  aboutTab.classList.remove('tab-selected');
+}
---
+++ src/load-page.js
@@ -0,0 +1,47 @@
+function loadPage() {
+  const content = document.getElementById('content');
+
+  createHeader();
+
+  function createHeader() {
+    const headerNode = document.createElement('header');
+    content.appendChild(headerNode);
+    const h1Node = document.createElement('h1');
+    headerNode.appendChild(h1Node);
+    const h1Text = document.createTextNode('NUTS AND BOLTS');
+    h1Node.appendChild(h1Text);
+
+    const nav = document.createElement('nav');
+    const ul = document.createElement('ul');
+    const li1 = document.createElement('li');
+    const li2 = document.createElement('li');
+    const li3 = document.createElement('li');
+
+    li1.setAttribute('id', 'main-tab');
+    li2.setAttribute('id', 'menu-tab');
+    li3.setAttribute('id', 'about-tab');
+
+    const li1Text = document.createTextNode('Main');
+    const li2Text = document.createTextNode('Menu');
+    const li3Text = document.createTextNode('About');
+
+    li1.appendChild(li1Text);
+    li2.appendChild(li2Text);
+    li3.appendChild(li3Text);
+    ul.appendChild(li1);
+    ul.appendChild(li2);
+    ul.appendChild(li3);
+    nav.appendChild(ul);
+    headerNode.appendChild(nav);
+
+    li1.classList.add('tab-selected');
+  }
+
+  const copyContainer = document.createElement('div');
+  copyContainer.setAttribute('id', 'copy-container');
+  content.appendChild(copyContainer);
+  // content.appendChild(myPic);
+  // content.appendChild(element);
+}
+
+export { loadPage };
